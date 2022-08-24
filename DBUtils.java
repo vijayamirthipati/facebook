7468a7b86c1c8baba99f6f510731c6abf4ef2e82
vijayamirthipati <vijjumirthipati208@gmail.com>
2022-08-24 18:29:21 +0530

    updated f

--- DBUtils.java
+++ DBUtils.java
@@ -3,5 +3,8 @@ hello guys am from devops branch
 
 hello guy
 
+milton thermosteel
+updated file in fetch
+
 
 this is the block of DBUtils in github.
--- EnveronmentalService.java
+++ EnveronmentalService.java
@@ -1,3 +1,6 @@
 hai am from developer branch......
+<<<<<<< HEAD
 master branch updated
+=======
+>>>>>>> ad24b18... master branch
 hai hello guys.
